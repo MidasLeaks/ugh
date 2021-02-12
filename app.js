64077d8fb21676c3ec5d726e2400d7c9cb886827
UEDeveloper <66185256+MidasLeaks@users.noreply.github.com>
2021-02-12 09:41:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 app.locals.pretty = true;
-app.set('port', process.env.PORT || @);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/app/server/views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
